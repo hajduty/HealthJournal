49a24800f6d9ac3842e2471f3b4af9c4826d5499
hajduty <hajduty@outlook.com>
2024-11-12 21:09:29 +0100

    add login endpoint

--- HealthJournal/Controllers/AccountController.cs
+++ HealthJournal/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using HealthJournal.Interfaces;
 using HealthJournal.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HealthJournal.Controllers
 {
@@ -11,57 +12,60 @@ namespace HealthJournal.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
         private readonly ITokenService _tokenService;
 
-        public AccountController(UserManager<User> userManager, ITokenService tokenService)
+        public AccountController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Email == loginDto.Email.ToLower());
+            
+            if (user == null) return Unauthorized("Invalid email");
+            
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            
+            if (!result.Succeeded) return Unauthorized("Password or email incorrect");
+            
+            var newUser = new NewUserDto{ Email = user.Email, Token = _tokenService.CreateToken(user)};
+            return Ok(newUser);
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
-            try
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new User
             {
-                if (!ModelState.IsValid)
-                    return BadRequest(ModelState);
+                Email = registerDto.Email,
+                UserName = registerDto.Email,
+            };
 
-                var user = new User
-                {
-                    Email = registerDto.Email,
-                    UserName = registerDto.Email,
-                };
+            var registeredUser = await _userManager.CreateAsync(user, registerDto.Password);
 
-                var registeredUser = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!registeredUser.Succeeded)
+                return BadRequest("User registration failed.");
 
-                if (registeredUser.Succeeded)
-                {
-                    var roleResult = await _userManager.AddToRoleAsync(user, "Patient");
+            var roleResult = await _userManager.AddToRoleAsync(user, "Patient");
 
-                    if (roleResult.Succeeded)
-                    {
-                        var userDto = new NewUserDto
-                        {
-                            Email = user.Email,
-                            Token = _tokenService.CreateToken(user)
-                        };
+            if (!roleResult.Succeeded)
+                return BadRequest("Role assignment failed.");
 
-                        return Ok(userDto);
-                    }
-
-
-                    return BadRequest(roleResult);
-                }
-                else
-                {
-                    return BadRequest(registeredUser);
-                }
-            }
-            catch (Exception ex)
+            var userDto = new NewUserDto
             {
-                return BadRequest(ex.Message);
-            }
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            };
+
+            return Ok(userDto);
         }
     }
 }
